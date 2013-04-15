38e1770c0a093845536600767e32548ecf71f050
zunda <zunda at freeshell.org>
2013-04-14 22:15:42 -1000

    Let test script follow the rename X|

--- test/test_githubapi.rb
+++ test/test_githubapi.rb
@@ -3,7 +3,7 @@ require 'test/unit'
 $:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'githubapi'
 
-include GitHub
+include GitHubApi
 
 class TestApi < Test::Unit::TestCase
 	def datapath(basename)
@@ -11,7 +11,7 @@ class TestApi < Test::Unit::TestCase
 	end
 
 	def test_timestamp
-		t = Api.new(datapath('SingleCommitComment.json'))
+		t = Base.new(datapath('SingleCommitComment.json'))
 		t.read_and_parse
 		assert_equal(Time.utc(2013,4,12,18,1,44), t.timestamp)
 	end
@@ -20,34 +20,34 @@ end
 class TestSingleCommitComment < Test::Unit::TestCase
 	def test_url
 		t = SingleCommitComment.new('zunda', 'test', '1')
-		assert_equal("#{Api::HOST}/repos/zunda/test/comments/1", t.url)
+		assert_equal("#{Base::HOST}/repos/zunda/test/comments/1", t.url)
 	end
 end
 
 class TestDownload < Test::Unit::TestCase
 	def test_url
 		t = Download.new('zunda', 'test', '1')
-		assert_equal("#{Api::HOST}/repos/zunda/test/downloads/1", t.url)
+		assert_equal("#{Base::HOST}/repos/zunda/test/downloads/1", t.url)
 	end
 end
 
 class TestGist < Test::Unit::TestCase
 	def test_url
 		t = Gist.new('1')
-		assert_equal("#{Api::HOST}/gists/1", t.url)
+		assert_equal("#{Base::HOST}/gists/1", t.url)
 	end
 end
 
 class TestSinglePullRequest < Test::Unit::TestCase
 	def test_url
 		t = SinglePullRequest.new('zunda', 'test', '1')
-		assert_equal("#{Api::HOST}/repos/zunda/test/pulls/1", t.url)
+		assert_equal("#{Base::HOST}/repos/zunda/test/pulls/1", t.url)
 	end
 end
 
 class TestCommit < Test::Unit::TestCase
 	def test_url
 		t = Commit.new('zunda', 'test', '1')
-		assert_equal("#{Api::HOST}/repos/zunda/test/commits/1", t.url)
+		assert_equal("#{Base::HOST}/repos/zunda/test/commits/1", t.url)
 	end
 end
