c3b79826c287d6ed54f56f90d80c4b185b64dced
zunda <zunda at freeshell.org>
2013-04-23 17:54:20 -1000

    Avoid occuping database too match
    Extraction of one hour of events takes about 10 seconds

--- scratch/create_markers.rb
+++ scratch/create_markers.rb
@@ -1,5 +1,5 @@
 #
-# usage: ruby scratch/create_markers.rb location-db-path event-db-path... > json
+# usage: ruby start-time end-time scratch/create_markers.rb location-db-path event-db-path... > json
 # Creates JSON list of markers from known events and locations
 #
 #
@@ -32,16 +32,26 @@ require 'geocoding'
 require 'sqlite3if'
 require 'marker'
 
+t0 = Time.parse(ARGV.shift)
+t1 = Time.parse(ARGV.shift)
+$stderr.puts "Extracting events from #{t0.utc} to #{t1.utc}"
+
 locationdb = SQLite3Database.open(ARGV.shift)
 
-markers = Markers.new
+events = Array.new
 ARGV.each do |eventdbpath|
 	eventdb = SQLite3Database.open(eventdbpath)
-	eventdb.retrieve('events', GitHubArchive::Event, '').each do |event|
-		location = locationdb.retrieve('locations', GoogleApi::Geocoding, 'WHERE address=?', event.location)[0]
-		if location and location.status == 'OK'
-			markers << Marker.new(event, location)
-		end
+	eventdb.retrieve('events', GitHubArchive::Event, 'WHERE ? <= timestamp AND timestamp < ?', t0.to_i, t1.to_i).each do |event|
+		events << event
 	end
 end
+
+markers = Markers.new
+events.each do |event|
+	location = locationdb.retrieve('locations', GoogleApi::Geocoding, 'WHERE address=?', event.location)[0]
+	if location and location.status == 'OK'
+		markers << Marker.new(event, location)
+	end
+end
+
 puts markers.to_json
