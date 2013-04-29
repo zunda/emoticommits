05d0f4b313a37255e3120640b74ae0df272817ef
zunda <zunda at freeshell.org>
2013-04-28 20:58:42 -1000

    Add license terms to Javascript files

--- javascripts/sun.js
+++ javascripts/sun.js
@@ -1,4 +1,31 @@
+//
+// Astronomical calculations are based on
 // 「天体の位置計算 増補版」長沢 工著、地人書館、2001年、ISBN4-8052-0225-4
+//
+// This software
+// Copyright (c) 2013 zunda <zunda at freeshell.org>
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+
 var Sun = {
 	et0_msec: new Date(Date.UTC(1974, 11, 31, 0)).getTime(),	// p.206
 	sin_deg: function(deg){return Math.sin(deg / 180.0 * Math.PI);},
